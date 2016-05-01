cf48ff2d04d53ef903499122e4022f6753853fff
Maks <maksdpua@gmail.com>
2016-05-01 16:18:24 +0300

    Enum2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -424,4 +424,29 @@ let a7 =
 
 countArray
 
+//enummark
+
+enum Action {
+    case Walk(meters: Int)
+    case Run(meters: Int, speed: Int)
+    case Stop
+    case Turn()
+}
+
+var action = Action.Run(meters: 20, speed: 15)
+
+action = .Stop
+
+action = .Walk(meters: 10)
+
+switch action {
+    
+    case .Stop: print("Stop")
+    case .Walk(let meters):
+    print("Walk \(meters) meters")
+    default: break
+}
+
+
+
 
